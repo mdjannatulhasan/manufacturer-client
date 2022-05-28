67daee0423525b632cab88d3d065ba1e47ce3ccd
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-05-28 14:37:25 +0600

    Profile page with edit and update, Order page with listing api

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,7 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyOrders = () => {
-    return <div>This Is My orders</div>;
+    const [user] = useAuthState(auth);
+    const [orders, SetOrders] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const { data } = await axios.get(`${process.env.REACT_APP_SERVER_URL}/myorders?email=${user?.email}`);
+            SetOrders(data);
+        };
+        getOrders();
+    }, []);
+    return (
+        <section className="bg-slate-100 pb-16 pt-10">
+            <div className="container">
+                <h2 className="text-2xl font-semibold">My orders</h2>
+            </div>
+        </section>
+    );
 };
 
 export default MyOrders;
--- src/Pages/MyProfile/MyProfile.js
+++ src/Pages/MyProfile/MyProfile.js
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState, useUpdateProfile } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
 import auth from "../../firebase.init";
-
 import userImagePlaceholder from "./../../image/User-Profile-PNG.png";
+
 const MyProfile = () => {
+    const [displayName, setDisplayName] = useState("");
+    const [photoURL, setPhotoURL] = useState("");
+    const [updateProfile, updating, error] = useUpdateProfile(auth);
     const [user] = useAuthState(auth);
     const [userInfo, setUserInfo] = useState({
+        _id: "",
         email: user.email,
         phone: "",
         location: { area: "", city: "", district: "", zipCode: "" },
         education: { degreeName: "", passingYear: "", institution: "" },
         socialLinks: { fb: "", instagram: "", linkedin: "" },
     });
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            const { data } = await axios.get(`${process.env.REACT_APP_SERVER_URL}/user?email=${user.email}`);
+            if (data) {
+                setUserInfo(data);
+            }
+            console.log(data);
+        };
+        fetchUserInfo();
+    }, []);
 
-    const userName = user?.displayName || "";
     const userEmail = user?.email || "";
     const phone = userInfo?.phone || "";
-    const locationArea = userInfo?.location.area || "";
+    const locationArea = userInfo?.location?.area || "";
     const locationCity = userInfo?.location?.city || "";
     const locationdistrict = userInfo?.location?.district || "";
     const locationZip = userInfo?.location?.zipCode || "";
@@ -26,13 +41,23 @@ const MyProfile = () => {
     const fb = userInfo?.socialLinks?.fb || "";
     const instagram = userInfo?.socialLinks?.instagram || "";
     const linkedin = userInfo?.socialLinks?.linkedin || "";
-    const handleInputChange = (e) => {};
-
-    const handleNameChange = (e) => {
-        const { displayName, ...rest } = userInfo;
-        const newUserInfo = { displayName: e.target.value, ...rest };
-        setUserInfo(newUserInfo);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const postUserInfo = async () => {
+            const { email, ...rest } = userInfo;
+            const newUserInfo = { email: user?.email, ...rest };
+            setUserInfo(newUserInfo);
+            console.log(userInfo);
+            const { data } = await axios.post(`${process.env.REACT_APP_SERVER_URL}/update-user`, newUserInfo);
+            if (data?.modifiedCount >= 1) {
+                toast.success("Data updated successfully");
+            } else if (data?.modifiedCount == 0) {
+                toast.info("There's nothing to update");
+            }
+        };
+        postUserInfo();
     };
+
     const handlePhoneChange = (e) => {
         const { phone, ...rest } = userInfo;
         const newUserInfo = { phone: e.target.value, ...rest };
@@ -82,28 +107,28 @@ const MyProfile = () => {
     };
     const handlefbChange = (e) => {
         const { fb, ...rest } = userInfo.socialLinks;
-        const { location, ...rest1 } = userInfo;
+        const { socialLinks, ...rest1 } = userInfo;
         const newUserInfo = { socialLinks: { fb: e.target.value, ...rest }, ...rest1 };
         setUserInfo(newUserInfo);
     };
     const handleInstaChange = (e) => {
         const { instagram, ...rest } = userInfo.socialLinks;
-        const { location, ...rest1 } = userInfo;
+        const { socialLinks, ...rest1 } = userInfo;
         const newUserInfo = { socialLinks: { instagram: e.target.value, ...rest }, ...rest1 };
         setUserInfo(newUserInfo);
     };
     const handleLinkedInChange = (e) => {
         const { linkedin, ...rest } = userInfo.socialLinks;
-        const { location, ...rest1 } = userInfo;
+        const { socialLinks, ...rest1 } = userInfo;
         const newUserInfo = { socialLinks: { linkedin: e.target.value, ...rest }, ...rest1 };
         setUserInfo(newUserInfo);
     };
-    const handlerofilePicChange = (e) => {
-        const { fb, ...rest } = userInfo.socialLinks;
-        const { location, ...rest1 } = userInfo;
-        const newUserInfo = { socialLinks: { fb: e.target.value, ...rest }, ...rest1 };
-        setUserInfo(newUserInfo);
+    const handleProfileForm = async (e) => {
+        e.preventDefault();
+        await updateProfile({ displayName, photoURL });
+        toast("Updated profile");
     };
+
     return (
         <div className="pb-16">
             <div className="cover-photo overflow-hidden">
@@ -115,21 +140,64 @@ const MyProfile = () => {
                     />
                 </figure>
             </div>
-            <div className="flex">
+            <div className="flex profile-wrapper">
                 <div className="profilepic rounded-full overflow-hidden w-[80px] lg:w-[100px] lg:h-[100px] h-[80px] m-1 border-2 border-white relative -top-6 left-9 bg-slate-200 z-10 shadow-md">
-                    <figure>
-                        <img className="bg-white object-cover h-[100px]" src={user?.photoURL || userImagePlaceholder} alt="" />
+                    <figure className="profile-pic">
+                        <img className="bg-white object-cover h-[100px]" src={user?.photoURL || photoURL || userImagePlaceholder} alt="" />
                     </figure>
                 </div>
                 <div className="username ml-14 pt-2">
                     <h2 className="font-bold text-xl ">{user?.displayName || "Your name"}</h2>
                     <h4 className="">{user?.email}</h4>
                 </div>
+                <label id="editProfile" className="hidden mt-4 ml-5 border-0 px-1 m-0 leading-none text-primary font-semibold cursor-pointer modal-button" htmlFor="profile">
+                    Edit <i className="fas fa-long-arrow-alt-right"></i>
+                </label>
+            </div>
+
+            <input type="checkbox" id="profile" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative rounded-md">
+                    <label htmlFor="profile" className="btn btn-sm btn-circle absolute right-2 top-2">
+                        ✕
+                    </label>
+                    <h3 className="font-semibold text-2xl">Edit Educational Information</h3>
+                    <form onSubmit={handleProfileForm} className="mt-5 space-y-2">
+                        <div className="flex items-center">
+                            <label htmlFor="userName" className="min-w-[150px] font-medium inline-block">
+                                Your Name:
+                            </label>
+                            <input
+                                id="userName"
+                                className="border px-3 py-1 bg-transparent col-span-9 w-full"
+                                type="displayName"
+                                value={displayName || user?.displayName}
+                                onChange={(e) => setDisplayName(e.target.value)}
+                                placeholder="Your latest degree name"
+                            />
+                        </div>
+                        <div className="flex items-center">
+                            <label htmlFor="PhotoUrl" className="min-w-[150px] col-span-3 font-medium inline-block">
+                                Photo Url:
+                            </label>
+                            <input
+                                id="PhotoUrl"
+                                className="border px-3 py-1 bg-transparent col-span-9 w-full"
+                                type="photoURL"
+                                value={photoURL || user?.photoURL}
+                                onChange={(e) => setPhotoURL(e.target.value)}
+                                placeholder="Photo URL"
+                                name="PhotoUrl"
+                            />
+                        </div>
+                        <input type="submit" value="Save" className="bg-primary py-1 px-6 rounded-sm inline-block text-white" />
+                    </form>
+                </div>
             </div>
             <div className="container">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
                     <div className="shadow-custom px-9 py-8 rounded-md">
-                        <div className="flex justify-between">
+                        <div className="flex justify-between items-center">
                             <h3 className="font-semibold text-2xl">Personal Information</h3>
                             <label className="border-0 px-1 m-0 leading-none text-primary font-semibold cursor-pointer modal-button" htmlFor="my-info">
                                 Edit <i className="fas fa-long-arrow-alt-right"></i>
@@ -140,7 +208,7 @@ const MyProfile = () => {
                                 <label htmlFor="userName" className="col-span-3 font-medium inline-block">
                                     Name:
                                 </label>
-                                <input id="userName" className="bg-transparent col-span-9 w-full" value={userName} type="text" placeholder="Your Name" disabled />
+                                <input id="userName" className="bg-transparent col-span-9 w-full" value={user?.displayName} type="text" placeholder="Your Name" disabled />
                             </div>
                             <div className="grid grid-cols-12 items-center">
                                 <label htmlFor="userEmail" className="col-span-3 font-medium inline-block">
@@ -157,7 +225,7 @@ const MyProfile = () => {
                         </form>
                     </div>
                     <div className="shadow-custom px-9 py-8 rounded-md">
-                        <div className="flex justify-between">
+                        <div className="flex justify-between items-center">
                             <h3 className="font-semibold text-2xl">Your Address</h3>
                             <label className="border-0 px-1 m-0 leading-none text-primary font-semibold cursor-pointer modal-button" htmlFor="my-location">
                                 Edit <i className="fas fa-long-arrow-alt-right"></i>
@@ -192,7 +260,7 @@ const MyProfile = () => {
                     </div>
                     <div className="shadow-custom px-9 py-8 rounded-md">
                         <div className="flex justify-between">
-                            <h3 className="font-semibold text-2xl">Educational Information</h3>
+                            <h3 className="font-semibold text-2xl items-center">Educational Information</h3>
                             <label className="border-0 px-1 m-0 leading-none text-primary font-semibold cursor-pointer modal-button" htmlFor="my-edu">
                                 Edit <i className="fas fa-long-arrow-alt-right"></i>
                             </label>
@@ -228,12 +296,12 @@ const MyProfile = () => {
                         ✕
                     </label>
                     <h3 className="font-semibold text-2xl">Edit Personal Information</h3>
-                    <form action="" className="mt-5 space-y-2">
+                    <form onSubmit={handleSubmit} className="mt-5 space-y-2">
                         <div className="grid grid-cols-12 items-center">
                             <label htmlFor="userName" className="col-span-3 font-medium inline-block">
                                 Name:
                             </label>
-                            <input id="userName" className="bg-transparent col-span-9 w-full" value={userName} type="text" placeholder="Your Name" disabled />
+                            <input id="userName" className="bg-transparent col-span-9 w-full" value={user?.displayName} type="text" placeholder="Your Name" disabled />
                         </div>
                         <div className="grid grid-cols-12 items-center">
                             <label htmlFor="userEmail" className="col-span-3 font-medium inline-block">
@@ -266,7 +334,7 @@ const MyProfile = () => {
                         ✕
                     </label>
                     <h3 className="font-semibold text-2xl">Edit Address</h3>
-                    <form action="" className="mt-5 space-y-2">
+                    <form onSubmit={handleSubmit} className="mt-5 space-y-2">
                         <div className="grid grid-cols-12 items-center">
                             <label htmlFor="locationArea" className="col-span-5 font-medium inline-block">
                                 Area/Road/Village:
@@ -317,7 +385,7 @@ const MyProfile = () => {
                         ✕
                     </label>
                     <h3 className="font-semibold text-2xl">Edit Educational Information</h3>
-                    <form action="" className="mt-5 space-y-2">
+                    <form onSubmit={handleSubmit} className="mt-5 space-y-2">
                         <div className="flex items-center">
                             <label htmlFor="userName" className="min-w-[150px] font-medium inline-block">
                                 Degree Name:
@@ -327,7 +395,7 @@ const MyProfile = () => {
                                 className="border px-3 py-1 bg-transparent col-span-9 w-full"
                                 value={degreeName}
                                 name="degreeName"
-                                onChange={handleInputChange}
+                                onChange={handleDegreeNameChange}
                                 type="text"
                                 placeholder="Your latest degree name"
                             />
@@ -340,7 +408,7 @@ const MyProfile = () => {
                                 id="pass-year"
                                 className="border px-3 py-1 bg-transparent col-span-9 w-full"
                                 value={passingYear}
-                                onChange={handleInputChange}
+                                onChange={handlePassYearChange}
                                 type="number"
                                 placeholder="Passing Year"
                                 name="passingYear"
@@ -356,12 +424,96 @@ const MyProfile = () => {
                                 value={institution}
                                 type="text"
                                 name="institution"
-                                onChange={handleInputChange}
+                                onChange={handleInstitutionChange}
                                 placeholder="Your Institution Name"
                             />
                         </div>
                         <input type="submit" value="Save" className="bg-primary py-1 px-6 rounded-sm inline-block text-white" />
                     </form>
+                </div>
+            </div>
+
+            <div className="container mt-8">
+                <div className="socials max-w-md">
+                    <div className="flex space-x-6 items-center">
+                        <h3 className="font-semibold text-2xl">Social Links</h3>
+                        <label className="border-0 px-1 m-0 leading-none text-primary font-semibold cursor-pointer modal-button" htmlFor="social">
+                            Add <i className="fas fa-long-arrow-alt-right"></i>
+                        </label>
+                    </div>
+                    <div className="text-2xl pt-4 space-x-4">
+                        {fb ? (
+                            <a href={fb}>
+                                <i class="fab fa-facebook-square text-blue-500"></i>
+                            </a>
+                        ) : (
+                            ""
+                        )}
+                        {instagram ? (
+                            <a href={instagram}>
+                                <i class="fab fa-instagram-square text-rose-800"></i>
+                            </a>
+                        ) : (
+                            ""
+                        )}
+                        <a href={linkedin}>
+                            <i class="fab fa-linkedin-in text-blue-800"></i>
+                        </a>
+                    </div>
+                    <input type="checkbox" id="social" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box relative rounded-md">
+                            <label htmlFor="social" className="btn btn-sm btn-circle absolute right-2 top-2">
+                                ✕
+                            </label>
+                            <h3 className="font-semibold text-2xl">Add social links</h3>
+                            <form onSubmit={handleSubmit} className="mt-5 space-y-2">
+                                <div className="flex items-center">
+                                    <label htmlFor="fb" className="min-w-[150px] font-medium inline-block">
+                                        Facebook:
+                                    </label>
+                                    <input
+                                        id="fb"
+                                        className="border px-3 py-1 bg-transparent col-span-9 w-full"
+                                        value={fb}
+                                        name="degreeName"
+                                        onChange={handlefbChange}
+                                        type="text"
+                                        placeholder="Facebook Link"
+                                    />
+                                </div>
+                                <div className="flex items-center">
+                                    <label htmlFor="pass-year" className="min-w-[150px] col-span-3 font-medium inline-block">
+                                        Instagram:
+                                    </label>
+                                    <input
+                                        id="pass-year"
+                                        className="border px-3 py-1 bg-transparent col-span-9 w-full"
+                                        value={instagram}
+                                        onChange={handleInstaChange}
+                                        type="text"
+                                        placeholder="Instagram Link"
+                                        name="instagram"
+                                    />
+                                </div>
+                                <div className="flex items-center">
+                                    <label htmlFor="Linkedin" className="min-w-[150px] font-medium inline-block">
+                                        Linkedin:
+                                    </label>
+                                    <input
+                                        id="Linkedin"
+                                        className="border px-3 py-1 bg-transparent col-span-9 w-full"
+                                        value={linkedin}
+                                        type="text"
+                                        name="Linkedin"
+                                        onChange={handleLinkedInChange}
+                                        placeholder="Linkedin Link"
+                                    />
+                                </div>
+                                <input type="submit" value="Save" className="bg-primary py-1 px-6 rounded-sm inline-block text-white" />
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
